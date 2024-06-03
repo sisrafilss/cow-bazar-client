8500d763494b514aa08a50ccfd2915ace3e13e50
IsrafilHossen3 <israfilhossen3@gmail.com>
2024-06-03 20:42:46 +0600

    testimonial section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../components/home/Banner";
+import Testimonial from "../components/home/Testimonial";
 import TopCows from "../components/home/TopCows";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <>
       <Banner />
       <TopCows />
+      <Testimonial />
     </>
   );
 };
