5ba95e973077ac6ef2457d04887846f72b3f5421
IsrafilHossen3 <israfilhossen3@gmail.com>
2024-06-04 05:31:47 +0600

    dashboard added

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,6 +3,9 @@ import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 import ContactUs from "../pages/ContactUs";
 import AboutUs from "../pages/AboutUs";
+import DashboardLayout from "../layouts/DashboardLayout";
+import ErrorPage from "../pages/ErrorPage";
+import Dashboard from "../pages/Dashboard/Dashboard";
 
 export const router = createBrowserRouter([
   {
@@ -24,19 +27,19 @@ export const router = createBrowserRouter([
       },
     ],
   },
-  // {
-  //   path: "dashboard",
-  //   element: <DashboardLayout />,
-  //   errorElement: <ErrorPage />,
-  //   children: [
-  //     {
-  //       path: "",
-  //       element: (
-  //         <PrivateRoute>
-  //           <Dashboard />
-  //         </PrivateRoute>
-  //       ),
-  //     },
-  //   ],
-  // },
+  {
+    path: "dashboard",
+    element: <DashboardLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: (
+          // <PrivateRoute>
+            <Dashboard />
+          // </PrivateRoute>
+        ),
+      },
+    ],
+  },
 ]);
